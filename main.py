3c548b4e65355e643f9a33b0880d6ee9b44106fa
Ziyao Wang <ziyaow36@gmail.com>
2020-12-12 01:51:02 -0500

    fix ecdc data

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from gs_quant.session import GsSession, Environment
 GsSession.use(Environment.PROD, '77d7c80dec0b44e9868dfaa3a7e2cb36', '4edbc70b2249de3ddc9f303bb373575cb06839fb6857570648fdb772ccf8e377', ('read_product_data',))
 
 ds = Dataset('COVID19_COUNTRY_DAILY_ECDC')
-data = ds.get_data()
+# data = ds.get_data()
 # print(data)  # peek at first few rows of data
+data = ds.get_data(datetime.date(2019, 12, 31))
 data.reset_index(inplace=True)
 data.to_json(r'ecdc.json')
